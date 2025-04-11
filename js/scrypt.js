2a640495a6217a159914011e846af33176ccba53
Jaume Murgadas <jmurgadas@gmail.com>
2025-04-11 15:33:31 +0200

    Tercer exercici fet

--- js/scrypt.js
+++ js/scrypt.js
@@ -44,19 +44,88 @@ class Hero {
     }
 }
 
+class Favorites {
+    constructor(){
+        this.favorites = [];
+    }
+
+    //MÈTODES
+
+    addFavorite(comic){
+        if (!this.favorites.find(com => com.id === comic.id)) {
+            this.favorites.push(comic);
+            console.log("Comic added to favorite list");
+        } else {
+            console.log("Comic is already in the list");
+        }
+    }
+
+    removeFavorite(comicId) {
+        if (this.favorites.find(com => com.id === comicId)){
+            this.favorites = this.favorites.filter(com => com.id !== comicId);
+            console.log("Comic deleted from favorite list");
+        } else {
+            console.log("Comic is not on favorite list");
+        }
+    }
+
+    showFavorites() {
+        this.favorites.forEach(
+            com => (console.log("ID : " + com.id + " - TITLE: " + com.title))
+        )
+    }
+
+    addMultipleFavorites(...comics){
+        comics.forEach(com => this.addFavorite(com));
+    }
+
+    copyFavorites() {
+        const savedList = new Favorites;
+        savedList.favorites = [...this.favorites];
+        return savedList;
+    }
+
+}
+
+/* CODI EXEMPLE*/
+
 const ruta = new Thumbnail("https://www.uoc.edu/comic_1_spiderman","jpg");
 const ruta2 = new Thumbnail("https://www.uoc.edu/foto_spiderman", "png");
+const ruta3 = new Thumbnail("https://www.uoc.edu/comic_1_hulk","jpg");
 const comic1 = new Comic(
     1,
     "Les aventures de l'increible Spiderman",
-    1-10,
+    10,
     "Primer episodi de la nova saga del elf verd",
     203,
     ruta,
-    "10€",
+    10,
     "Jaume Murgadas",
     "Spiderman, Elf Verd, Dona aranya"
 );
+const comic2 = new Comic(
+    2,
+    "Les aventures de l'increible Hulk",
+    12,
+    "Primer episodi de la nova saga del monstre verd",
+    120,
+    ruta2,
+    5,
+    "Jaume Murgadas",
+    "Hulk, Hulka, Hulkets"
+);
+const comic3 = new Comic(
+    24,
+    "Les aventures de l'increible Hulk a Catalunya",
+    34,
+    "Últim episodi de la nova saga del monstre verd",
+    178,
+    ruta2,
+    15,
+    "Jaume Murgadas",
+    "Hulk, Hulka, Hulkets"
+);
+
 const hero1 = new Hero(
     3,
     "Spiderman",
@@ -67,5 +136,8 @@ const hero1 = new Hero(
     comic1 
 );
 
-console.log(hero1.getThumbnailURL());
+let lista = new Favorites();
+lista.addMultipleFavorites(comic1,comic2,comic3);
+lista.showFavorites();
+console.log(lista.length);
 
